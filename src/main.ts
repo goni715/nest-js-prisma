91e57230c7fb1c368c8099bea604ce44e206fa7c
goni715 <halesgoni715149123@gmail.com>
2026-06-02 22:14:56 +0600

    exception filter added

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 /* eslint-disable @typescript-eslint/no-floating-promises */
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Request, Response } from 'express';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
